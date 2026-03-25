4910e2cbb500e0265945b831140cc080a8e6dfc4
shrutikapawar2613 <shrutikapawar2613@gmail.com>
2026-03-25 12:35:40 +0530

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,46 @@
+package collectionframework.wrapper.Array;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+	
+//		int arr[]= {10,20,30,40};
+//		//normal for loop
+//		for(int i=0;i<arr.length;i++) {
+//			System.out.println(arr[i]);
+//		}
+//		
+//		//enhanced for loop
+//		for(int a:arr) {
+//			System.out.println(a);
+//		}
+		
+		ArrayList obj=new ArrayList();
+		
+		obj.add(10);
+		obj.add(true);
+		obj.add("hello");
+		obj.add(2,40);
+		System.out.println(obj);
+		//System.out.println(obj.get(3));
+		
+		//1.normal for loop
+//		for(int i=0;i<obj.size();i++) {
+//			System.out.println(obj.get(i));
+//		}
+//		System.out.println("-------------------------------------------");
+		
+		//2.enhanced for loop
+//     	for(object a :obj) {
+//			System.out.println(a);
+//		}
+		
+		//3.for each loop
+		//function programming method -loop
+//		obj.forEach(val->System.out.println(val));
+//		
+//		obj.forEach(system.out::println);
+	}
+}
